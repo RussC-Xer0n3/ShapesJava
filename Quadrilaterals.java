c96d50506d0fa500797a6d43c8aec26c5053eb47
Xer0n3 - RussC <developing821@outlook.com>
2023-02-27 17:18:35 +0000

    Update Quadrilaterals.java
    Completed some additional calculations lines 17-19 two new variables, lines 247-311, corrected, new methods added for parallel, opposite and trapezium area

--- Quadrilaterals.java
+++ Quadrilaterals.java
@@ -1,5 +1,7 @@
 package ShapesJava;
 
+import Coordinates.Distance;
+
 /**
  * A class to handle quadrilaterals of most types
  * @author xer0n3 - developing821@outlook.com - R.Clarke
@@ -11,6 +13,9 @@ public class Quadrilaterals {
 	private static double bottom;
 	private static double left;
 	private static double right;
+	
+	private static double height; // For use with opposite() and parallel()
+	private static double width; // For use with opposite()
 	
 	private static double top_left_angle;
 	private static double bottom_left_angle;
@@ -81,7 +86,7 @@ public class Quadrilaterals {
 	 */
 	public static boolean square() {
 		
-		if (quadrilateral() == true && all_sides_equal() == true && all_angles_equal() == true) {
+		if (quadrilateral() == true && all_sides_equal() == true && all_angles_equal() == true && opposite() == true && parallel() == true) {
 			return true;
 		} else {
 			return false;
@@ -125,9 +130,9 @@ public class Quadrilaterals {
 	 */
 	public static boolean parallelogram() {
 		
-		if (quadrilateral() == true && top_left_angle < 90 && bottom_left_angle > 90 && top_right_angle > 90 && bottom_right_angle < 90) {
+		if (quadrilateral() == true && parallel() == true && top_left_angle < 90 && bottom_left_angle > 90 && top_right_angle > 90 && bottom_right_angle < 90) {
 			return true;
-		} else if (quadrilateral() == true && top_left_angle > 90 && bottom_left_angle < 90 && top_right_angle < 90 && bottom_right_angle > 90) {
+		} else if (quadrilateral() == true && parallel() == true && top_left_angle > 90 && bottom_left_angle < 90 && top_right_angle < 90 && bottom_right_angle > 90) {
 			return true;
 		} else {
 			return false;
@@ -136,12 +141,68 @@ public class Quadrilaterals {
 	}
 	
 	/**
+	 * Using height, creates a boolean of distance greater than 0 and 
+	 * perpendicular to each other for top and bottom
+	 * @return
+	 */
+	public static boolean parallel() {
+		
+		Distance.setPoint(top / 2);
+		Distance.setPoint_b(height);
+		
+		if (height != 0 && Distance.distance() == height) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Using height, creates a boolean of distance greater than 0 and 
+	 * perpendicular to each other for top and bottom
+	 * @return
+	 */
+	public static boolean opposite() {
+		
+		Distance.setPoint(top / 2);
+		Distance.setPoint_b(height);
+		
+		if (height != 0 && Distance.distance() == height) {
+			return true;
+		}
+		
+		Distance.setPoint(bottom / 2);
+		Distance.setPoint_b(height);
+		
+		if (height != 0 && Distance.distance() == height) {
+			return true;
+		}
+		
+		Distance.setPoint(left / 2);
+		Distance.setPoint_b(width);
+		
+		if (width != 0 && Distance.distance() == width) {
+			return true;
+		}
+		
+		Distance.setPoint(right / 2);
+		Distance.setPoint_b(width);
+		
+		if (width != 0 && Distance.distance() == width) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+	/**
 	 * Checks if the quadrilateral is an irregular trapezium
 	 * @return
 	 */
 	public static boolean trapezium() {
 		
-		if (quadrilateral() == true && all_sides_equal() == false && all_angles_equal() == false && two_angles_equal() == false && top != bottom && left != right) {
+		if (quadrilateral() == true && all_sides_equal() == false && all_angles_equal() == false && two_angles_equal() == false && top != bottom && left != right && parallel() == true) {
 			return true;
 		} else {
 			return false;
@@ -155,12 +216,12 @@ public class Quadrilaterals {
 	 */
 	public static boolean isosceles_trapezium() {
 		
-		if (quadrilateral() == true && all_sides_equal() == false && all_angles_equal() == false && two_angles_equal() == true) {
+		if (quadrilateral() == true && all_sides_equal() == false && all_angles_equal() == false && two_angles_equal() == true && parallel() == true) {
 			if (bottom_right_angle == bottom_left_angle && top_right_angle == top_left_angle && left == right) {
 				return true;
 			}
 			return true;
-		} else if (quadrilateral() == true && all_sides_equal() == false && all_angles_equal() == false && two_angles_equal() == true) {
+		} else if (quadrilateral() == true && all_sides_equal() == false && all_angles_equal() == false && two_angles_equal() == true && parallel() == true) {
 			if (bottom_left_angle == top_left_angle && top_right_angle == bottom_right_angle && top == bottom) {
 				return true;
 			}
@@ -237,21 +298,24 @@ public class Quadrilaterals {
 	}
 	
 	/**
-	 * Provides the diagonal of the rectangle after running the quadrilateral and rectangle checks
+	 * Provides the diagonal specifications of the rectangle after running the quadrilateral and rectangle checks
 	 * @return
 	 */
 	public static double rectangle_diagonal() { 
 		double diagonal = 0;
 		
 		if (quadrilateral() == true && rectangle() == true) {
-			diagonal = Math.sqrt(Math.pow(left, 2) + Math.pow(bottom, 2));
+			Triangle.setAdjacent(bottom);
+			Triangle.setOpposite(left);
+			Triangle.right_triangle_edges();
+			diagonal = Triangle.getHypotenuse();
 		}
 		
 		return diagonal;
 	}
 	
 	/**
-	 * Provides the area of the square after running the quadrilateral and square checks
+	 * Provides the area of the rectangle after running the quadrilateral and square checks
 	 * @return
 	 */
 	public static double rectangle_area() {
@@ -275,8 +339,38 @@ public class Quadrilaterals {
 		
 		double perimeter = 0;
 		
-		if (quadrilateral() == true && square() == true) {
-			perimeter = Math.pow(left, 2) + Math.pow(bottom, 2);
+		if (quadrilateral() == true && rectangle() == true) {
+			perimeter = top+bottom+left+right;
+		}
+		
+		return perimeter;
+	}
+	
+	/**
+	 * Provides the area of the trapezium after running the quadrilateral and square checks
+	 * @return
+	 */
+	public static double trapezium_area() {
+		
+		double area = 0;
+		
+		if (quadrilateral() == true && trapezium() == true) {
+			area = Math.pow((top + bottom / 2), height); 
+		}
+		
+		return area;
+	}
+	
+	/**
+	 * Provides the perimeter of the trapezium after performing the quaderilateral and rectangle checks
+	 * @return
+	 */
+	public static double trapezium_perimeter() {
+		
+		double perimeter = 0;
+		
+		if (quadrilateral() == true && trapezium() == true) {
+			perimeter = top+bottom+left+right;
 		}
 		
 		return perimeter;
@@ -299,7 +393,6 @@ public class Quadrilaterals {
 			return false;
 		}
 	}
-	
 	
 	
 	
@@ -327,29 +420,53 @@ public class Quadrilaterals {
 	public static void setRight(double right) {
 		Quadrilaterals.right = right;
 	}
-	public static double getTop_angle() {
+	
+	public static double getHeight() {
+		return height;
+	}
+
+	public static void setHeight(double height) {
+		Quadrilaterals.height = height;
+	}
+	
+	public static double getWidth() {
+		return width;
+	}
+
+	public static void setWidth(double width) {
+		Quadrilaterals.width = width;
+	}
+
+	public static double getTop_left_angle() {
 		return top_left_angle;
 	}
-	public static void setTop_angle(double top_angle) {
-		Quadrilaterals.top_left_angle = top_angle;
+
+	public static void setTop_left_angle(double top_left_angle) {
+		Quadrilaterals.top_left_angle = top_left_angle;
 	}
-	public static double getBottom_angle() {
+
+	public static double getBottom_left_angle() {
 		return bottom_left_angle;
 	}
-	public static void setBottom_angle(double bottom_angle) {
-		Quadrilaterals.bottom_left_angle = bottom_angle;
+
+	public static void setBottom_left_angle(double bottom_left_angle) {
+		Quadrilaterals.bottom_left_angle = bottom_left_angle;
 	}
-	public static double getLeft_angle() {
+
+	public static double getTop_right_angle() {
 		return top_right_angle;
 	}
-	public static void setLeft_angle(double left_angle) {
-		Quadrilaterals.top_right_angle = left_angle;
+
+	public static void setTop_right_angle(double top_right_angle) {
+		Quadrilaterals.top_right_angle = top_right_angle;
 	}
-	public static double getRight_angle() {
+
+	public static double getBottom_right_angle() {
 		return bottom_right_angle;
 	}
-	public static void setRight_angle(double right_angle) {
-		Quadrilaterals.bottom_right_angle = right_angle;
+
+	public static void setBottom_right_angle(double bottom_right_angle) {
+		Quadrilaterals.bottom_right_angle = bottom_right_angle;
 	}
 	
 }
